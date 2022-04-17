939a197b0172848e9eb320fe7419b3b8aa59043a
Miguelmlj <hsmiguelhs@gmail.com>
2022-04-16 21:31:05 -0600

    cambios en fecha, comprobar conexiones wifi

--- app/src/main/java/com/example/bd_inventario/Retrofit/Utilidades.java
+++ app/src/main/java/com/example/bd_inventario/Retrofit/Utilidades.java
@@ -2,7 +2,8 @@ package com.example.bd_inventario.Retrofit;
 
 public class Utilidades {
     //url general de la api, cambiará cuando se monte al server
-    public static final String BASE_URL = "http://10.10.10.52:3000/";
+//    public static final String BASE_URL = "http://10.10.10.52:3000/"; //trabajo
+    public static final String BASE_URL = "http://192.168.0.13/:3000/"; //casa
 
     //metodo para retornar el cliente retrofit
     public static apiRest getAPIService(){
